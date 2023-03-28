15365f232b3529e88866b43ef4dfae1addc7bbb6
lookman1 <luqmanul312@gmail.com>
2023-03-29 06:30:12 +0700

    update

--- lib/components/snackbars.dart
+++ lib/components/snackbars.dart
@@ -131,43 +131,43 @@ class Snackbars {
   }
 }
 
-  // helpSnackbars(BuildContext context, String title, String message) {
-  //   SmartSnackBars.showTemplatedSnackbar(
-  //     elevation: 2,
-  //     animationCurve: Curves.fastLinearToSlowEaseIn,
-  //     leading: Container(
-  //       margin: const EdgeInsets.only(right: 10),
-  //       padding: const EdgeInsets.all(10),
-  //       decoration: BoxDecoration(
-  //         shape: BoxShape.circle,
-  //         color: Colors.white.withOpacity(0.2),
-  //       ),
-  //       child: const Icon(
-  //         Icons.info_outline_sharp,
-  //         color: Colors.white,
-  //       ),
-  //     ),
-  //     animateFrom: AnimateFrom.fromBottom,
-  //     duration: const Duration(milliseconds: 3500),
-  //     backgroundColor: Colors.blue,
-  //     titleWidget: Text(
-  //       title,
-  //       style: const TextStyle(
-  //           color: Colors.white,
-  //           fontFamily: 'Poppins',
-  //           fontWeight: FontWeight.w700),
-  //     ),
-  //     subTitleWidget: Padding(
-  //       padding: const EdgeInsets.only(top: 8.0),
-  //       child: Text(
-  //         message,
-  //         style: const TextStyle(
-  //           color: Colors.white,
-  //           fontWeight: FontWeight.w600,
-  //           fontSize: 14,
-  //         ),
-  //       ),
-  //     ),
-  //     context: context,
-  //   );
-  // }
+helpSnackbars(BuildContext context, String title, String message) {
+  SmartSnackBars.showTemplatedSnackbar(
+    // elevation: 2,
+    animationCurve: Curves.fastLinearToSlowEaseIn,
+    leading: Container(
+      margin: const EdgeInsets.only(right: 10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white.withOpacity(0.2),
+      ),
+      child: const Icon(
+        Icons.info_outline_sharp,
+        color: Colors.white,
+      ),
+    ),
+    animateFrom: AnimateFrom.fromBottom,
+    duration: const Duration(milliseconds: 3500),
+    backgroundColor: Colors.blue,
+    titleWidget: Text(
+      title,
+      style: const TextStyle(
+          color: Colors.white,
+          fontFamily: 'Poppins',
+          fontWeight: FontWeight.w700),
+    ),
+    subTitleWidget: Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Text(
+        message,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w600,
+          fontSize: 14,
+        ),
+      ),
+    ),
+    context: context,
+  );
+}
